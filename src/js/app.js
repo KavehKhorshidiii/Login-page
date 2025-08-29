50c840122abfe347a2c77532241c22e90cb0d882
Kaveh Khorshidi <kavehkhorshidiii@gmail.com>
2025-08-29 22:01:17 +0330

    onClick function

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,6 @@
-console.log("object")
+let PassInput = document.getElementsByClassName("PassInput")
+
+PassInput[0].addEventListener(onclick , () => {
+    console.log("object")
+})
+console.log(PassInput[0])
