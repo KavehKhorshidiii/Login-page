015aca0f6a99c494bbefe2080ec40a963e159b35
Kaveh Khorshidi <kavehkhorshidiii@gmail.com>
2025-08-29 22:47:50 +0330

    update

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,10 @@ let PassInput = document.getElementsByClassName("PassInput")
 let showPassBtn = document.getElementsByClassName("showPassBtn")
 
 showPassBtn[0].addEventListener("click" , () => {
+    
     PassInput[0].type = PassInput[0].type === 'password' ? 'text' : "password"
     console.log(PassInput[0].type)
-    
+
 })
 
 
