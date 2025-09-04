94805d4c3564a8e40214303ccecdb1e6cd3fcca2
Kaveh Khorshidi <kavehkhorshidiii@gmail.com>
2025-09-04 23:26:03 +0330

    update

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,9 @@
 let PassInput = document.getElementsByClassName("PassInput")
 let showPassBtn = document.getElementsByClassName("showPassBtn")
 let loginBtn = document.getElementById("loginBtn")
-let eyeSVG = document.getElementById()
-let closeEyeSVG = document.getElementById()
+let eyeSVG = document.getElementById("eye")
+let closeEyeSVG = document.getElementById("closeEye")
+
 //show password 
 showPassBtn[0].addEventListener("click" , () => {
     PassInput[0].type = PassInput[0].type === 'password' ? 'text' : "password"
